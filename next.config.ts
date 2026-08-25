ba2390cd8d64ffceea54f96d6b81f3c2c62a1296
Tobi Schneider <tobischneider1@hotmail.com>
2026-08-25 19:05:42 +0200

    Build Befehl angepasst

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Ignoriert TypeScript-Fehler beim Build
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Ignoriert ESLint-Fehler beim Build
-    ignoreDuringBuilds: true,
   },
 };
 
